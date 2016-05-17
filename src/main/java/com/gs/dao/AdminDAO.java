325f7e2fcaa6b59e5ce6a9e76429b6587f6afc57
Wgssmart <847315251@qq.com>
2016-05-17 22:24:02 +0800

    DAO加上@Repository注解

--- src/main/java/com/gs/dao/AdminDAO.java
+++ src/main/java/com/gs/dao/AdminDAO.java
@@ -1,10 +1,12 @@
 package com.gs.dao;
 
 import com.gs.bean.Admin;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by WangGenshen on 5/16/16.
  */
+@Repository
 public interface AdminDAO extends BaseDAO<Admin, String> {
 
 }
--- src/main/java/com/gs/dao/CustomerDAO.java
+++ src/main/java/com/gs/dao/CustomerDAO.java
@@ -1,10 +1,12 @@
 package com.gs.dao;
 
 import com.gs.bean.Customer;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by WangGenshen on 5/16/16.
  */
+@Repository
 public interface CustomerDAO extends BaseDAO<Customer,String> {
 
     public Customer queryByEmailPwd(Customer customer);
