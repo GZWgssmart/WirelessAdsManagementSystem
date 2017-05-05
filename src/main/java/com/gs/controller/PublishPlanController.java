49769e3fc63bca849158d026d775435ee7024b0f
WangGenshen <847315251@qq.com>
2017-05-05 17:33:09 +0800

    ADSServerV2版本

--- src/main/java/com/gs/controller/PublishPlanController.java
+++ src/main/java/com/gs/controller/PublishPlanController.java
@@ -13,6 +13,7 @@ import com.gs.common.web.ADSServerUtil;
 import com.gs.common.web.SessionUtil;
 import com.gs.net.parser.Common;
 import com.gs.net.server.ADSServer;
+import com.gs.net.server.ADSServerV2;
 import com.gs.service.CustomerService;
 import com.gs.service.PublishPlanService;
 import com.gs.service.PublishService;
@@ -280,7 +281,7 @@ public class PublishPlanController {
                     // 一旦审核,则需要通知客户端下载文件,并完成发布操作，只有完成发布操作后，整个审核才算完毕
                     // 查找单个计划，及此计划下的所有终端,每一个终端都要开始发送文件下载通知
                     List<Publish> publishs = publishService.queryByPlanId(id);
-                    ADSServer adsServer = ADSServerUtil.getADSServerFromServletContext();
+                    ADSServerV2 adsServer = ADSServerUtil.getADSServerFromServletContext();
                     for (Publish publish : publishs) {
                         adsServer.writeFileDownload(publish);
                     }
